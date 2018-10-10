83bb89a7caa2d6d4881d708565aead3d8bfbd6e4
colin27091 <colinregourd@gmail.com>
2018-10-10 16:37:21 +0200

    Fin HSQL Banking

--- src/test/java/banking/BankingTest.java
+++ src/test/java/banking/BankingTest.java
@@ -60,7 +60,57 @@ public class BankingTest {
 		assertEquals("Balance incorrecte !", before0 - amount, myDAO.balanceForCustomer(fromCustomer), 0.001f);
 		assertEquals("Balance incorrecte !", before1 + amount, myDAO.balanceForCustomer(toCustomer), 0.001f);				
 	}
-	
+        
+        @Test
+	public void invalidBalance() throws SQLException, Exception{
+            float amount = 110.0f;
+            int fromCustomer = 0;
+            int toCustomer = 1;
+            float before0 = myDAO.balanceForCustomer(fromCustomer);
+            float before1 = myDAO.balanceForCustomer(toCustomer);
+            
+            try {
+                myDAO.bankTransferTransaction(fromCustomer, toCustomer, amount);
+            }
+            catch (SQLException ex) {
+                assertEquals("Erreur le customer n'as pas assez d'argent", before0, myDAO.balanceForCustomer(fromCustomer), 0.01f);
+                assertEquals("Erreur le customer n'as pas assez d'argent", before1,myDAO.balanceForCustomer(toCustomer), 0.01f);
+                return;
+            }
+            fail();
+        }
+        
+        
+        @Test
+        public void invalidCustomerRECEIVE() throws SQLException, Exception{
+            float amount = 50.0f;
+            int fromCustomer = 0;
+            int toCustomer = 2;
+            float before0 = myDAO.balanceForCustomer(fromCustomer);
+            try {
+                myDAO.bankTransferTransaction(fromCustomer, toCustomer, amount);
+            } catch (Exception e) {
+                assertEquals("Erreur le customer n'existe pas", before0, myDAO.balanceForCustomer(fromCustomer), 0.01f);
+                return;
+            }
+            fail();
+        }
+        
+        @Test
+        public void invalidCustomerSEND() throws SQLException, Exception{
+            float amount = 50.0f;
+            int fromCustomer = 2;
+            int toCustomer = 1;
+            float before0 = myDAO.balanceForCustomer(fromCustomer);
+            try {
+                myDAO.bankTransferTransaction(fromCustomer, toCustomer, amount);
+            } catch (Exception e) {
+                assertEquals("Erreur le customer n'existe pas", before0, myDAO.balanceForCustomer(fromCustomer), 0.01f);
+                return;
+            }
+            fail();
+        }
+        
 
 	public static DataSource getDataSource() throws SQLException {
 		org.hsqldb.jdbc.JDBCDataSource ds = new org.hsqldb.jdbc.JDBCDataSource();
@@ -68,5 +118,5 @@ public class BankingTest {
 		ds.setUser("sa");
 		ds.setPassword("sa");
 		return ds;
-	}	
+	}
 }
